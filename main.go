339b5d1c66c3fb33eee7eae87a30c71cb900f22f
Tsyren <nsu1team@gmail.com>
2021-11-14 14:07:31 +0800

    create and query examples

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/Funfun/entgo-example/ent"
+	playerEnt "github.com/Funfun/entgo-example/ent/player"
 	_ "github.com/lib/pq"
 )
 
@@ -17,4 +22,38 @@ func main() {
 	defer client.Close()
 
 	log.Println("Connected to db")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Dump migration changes to stdout.
+	if err := client.Schema.WriteTo(ctx, os.Stdout); err != nil {
+		log.Fatalf("failed printing schema changes: %v", err)
+	}
+
+	log.Println("press key <Enter> to continue")
+	fmt.Scanln()
+
+	if err := client.Schema.Create(ctx); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	// create a Player
+	player, err := client.Player.Create().
+		SetNickname("John").
+		SetEmail("info@tsyren.org").
+		SetScores(1).
+		Save(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("New player was created", player)
+
+	player, err = client.Player.Query().Where(playerEnt.Nickname("John")).Only(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Found player was created", player)
 }
