381e09e50b3bf411147c4601fabf06a5283245b5
Tsyren <nsu1team@gmail.com>
2021-11-14 13:26:55 +0800

    use ent to connect to db

--- main.go
+++ main.go
@@ -1,25 +1,20 @@
 package main
 
 import (
-	"database/sql"
 	"log"
 
+	"github.com/Funfun/entgo-example/ent"
 	_ "github.com/lib/pq"
 )
 
 func main() {
 	log.Println("Welcome to Entgo example article")
 
-	db, err := sql.Open("postgres", "user=postgres password=topsecret dbname=entgo_example sslmode=disable")
+	client, err := ent.Open("postgres", "user=postgres password=topsecret dbname=entgo_example sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
+	defer client.Close()
 
 	log.Println("Connected to db")
 }
